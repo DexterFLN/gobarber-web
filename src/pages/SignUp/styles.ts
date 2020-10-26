89ec49ab73127b8d908a847f993cfc494c79fdcb
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-10-26 01:55:50 -0300

    Creating routes

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,10 +1,11 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
+
   display: flex;
   align-items: stretch;
 `;
@@ -13,16 +14,40 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  place-content: center;
+  justify-content: center;
+
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
+
   form {
     margin: 40px 0;
     width: 340px;
     text-align: center;
+
     h1 {
       margin-bottom: 24px;
     }
+
     a {
       color: #F4EDE8;
       display: block;
@@ -35,7 +60,7 @@ export const Content = styled.div`
     }
   }
   > a {
-    color: #F4EDE8;
+    color: #FF9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -46,7 +71,7 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#F4EDE8')};
+      color: ${shade(0.2, '#FF9000')};
     }
   }
 `;
